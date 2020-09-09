d9752603f8457a477dec0fd92a5bc8bea43477ff
dalejac <k-da929@hotmail.com>
2020-09-09 09:35:09 -0500

    Commit

--- src/lib/views/sign.js
+++ src/lib/views/sign.js
@@ -12,7 +12,7 @@ export function SignIn () {
         <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-2.54,146.53 C50.51,172.20 209.65,-13.31 500.84,141.61 L508.17,-34.03 L-1.41,-37.00 Z" style="stroke: none; fill: #229ACF;"></path></svg></div>`
     view.appendChild(header);
 
-    let form = document.createElement("form")
+    let form = document.createElement("form");
     form.innerHTML = `
     <input type="text" id="name" placeholder="Name">
     <input type="email" id="user" placeholder="E-mail">
